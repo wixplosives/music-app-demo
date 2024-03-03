587bad38447fb329024dfed0dc5769b642d7dcf8
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-03-03 14:45:16 +0200

    Added play button. Titles margin changed.

--- src/components/mini-player/mini-player.tsx
+++ src/components/mini-player/mini-player.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import styles from './mini-player.module.scss';
+import { PlayBtn } from '../play-btn/play-btn';
 
 export interface MiniPlayerProps {
     className?: string;
@@ -27,6 +28,7 @@ export const MiniPlayer = ({ className }: MiniPlayerProps) => {
                         </div>
                     </div>
                     <div className={styles.icons}>
+                        <PlayBtn variant="primary" />
                         <img
                             src="https://i.ibb.co/fvMWN0Q/Heart.png "
                             alt=""
