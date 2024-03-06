121e644555d4c132a8bfeedc72c288769fde3ed3
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-03-06 17:18:30 +0200

    Added component and styles changes.

--- src/components/new-releases/new-releases.tsx
+++ src/components/new-releases/new-releases.tsx
@@ -26,7 +26,7 @@ export const NewReleases = ({ className }: NewReleasesProps) => {
                     song="2050"
                 />
                 <ReleaseCard
-                    imageUrl="https://images.unsplash.com/photo-1594315590298-329f49c8dcb9?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    imageUrl="https://static.wixstatic.com/media/27eba2_bea1b50b08bc4d72a8d01a4134aba985~mv2.png"
                     artist="The Kicking Hills"
                     song="A New Planet"
                 />
